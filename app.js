adc91a2d71008ba89f85f053a0d8cfb236cb7da3
Heather Hern <heathernewlin1@gmail.com>
2020-10-20 14:58:36 -0400

    copied from wrong repo
    Added dependencies, port, data parsing, routes, listener

---
+++ app.js
@@ -0,0 +1,38 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// =============================================================
+// * The following HTML routes should be created:
+
+// Basic route that sends the user first to the Index Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+//   * GET `/notes` - Should return the `notes.html` file.
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+//   * GET `*` - Should return the `index.html` file
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
