f2a7a78ef7321453b67c52414164d175c50f47f3
Heather Hern <heathernewlin1@gmail.com>
2020-10-21 15:01:59 -0400

    Added GET `/api/notes`

--- app.js
+++ app.js
@@ -36,3 +36,28 @@ app.get("*", function (req, res) {
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
+
+// * The following API routes should be created:
+
+//   * GET `/api/notes` - Should read the `db.json` file and return all 
+//   saved notes as JSON.
+
+const apiRoutes = function(app){
+    app.get("/api/notes", function(req, res){
+        fs.readFile(dbFilePath, "utf8", function(err, data){
+            console.log("Running 'Get db.json'");
+            res.json(data);
+        });
+    });
+
+
+//   * POST `/api/notes` - Should receive a new note to save on the 
+//   request body, add it to the `db.json` file, and then return the 
+//   new note to the client.
+
+//   * DELETE `/api/notes/:id` - Should receive a query parameter 
+//   containing the id of a note to delete. This means you'll need 
+//   to find a way to give each note a unique `id` when it's saved.
+//    In order to delete a note, you'll need to read all notes from 
+//    the `db.json` file, remove the note with the given `id` property, 
+//    and then rewrite the notes to the `db.json` file.
