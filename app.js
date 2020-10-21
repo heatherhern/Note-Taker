dd9bca74c8d68cd8cfd4ee20f2d1a7d47f795c62
Heather Hern <heathernewlin1@gmail.com>
2020-10-21 14:58:54 -0400

    Added db.json

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -17,17 +17,17 @@ app.use(express.json());
 
 // Basic route that sends the user first to the Index Page
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "develop/public/index.html"));
 });
 
 //   * GET `/notes` - Should return the `notes.html` file.
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "develop/public/notes.html"));
 });
 
 //   * GET `*` - Should return the `index.html` file
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "develop/public/index.html"));
 });
 
 
